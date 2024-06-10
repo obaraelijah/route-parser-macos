a30d8fc3bf2b66f932cbc0ba50771f3498dd89f8
obaraelijah <elijahobara357@gmail.com>
2024-06-10 14:19:07 +0300

    feat: Parser & document code

--- src/route_entry.rs
+++ src/route_entry.rs
@@ -5,16 +5,28 @@ use crate::{Destination, Protocol, RoutingFlag};
 /// A single route obtained from the `netstat -rn` output
 #[derive(Debug, Clone)]
 pub struct RouteEntry {
+    /// Protocol
     pub proto: Protocol,
+
+    /// Destination.  E.g., a host or CIDR
     pub dest: Destination,
+
+    /// Gateway (i.e., how to reach the destination)
     pub gateway: Destination,
+
+    /// Routing flags
     pub flags: HashSet<RoutingFlag>,
+
+    /// Network interface that holds this route
     pub net_if: String,
+
+    /// RouteEntry expiration.  This is primarily seen for ARP-derived entries
     pub expires: Option<Duration>,
 }
 
 impl std::fmt::Display for RouteEntry {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        #[allow(unused_variables)]
         let RouteEntry {
             proto,
             dest,
@@ -68,7 +80,21 @@ pub enum Error {
 
 impl RouteEntry {
     pub(crate) fn parse(proto: Protocol, line: &str) -> Result<Self, Error> {
-        todo!()
+        let flags = HashSet::new();
+        let dest = None;
+        let gateway = None;
+        let net_if: Option<String> = None;
+        let expires = None;
+
+        let route = RouteEntry {
+            proto,
+            dest: dest.ok_or(Error::MissingDestination)?,
+            gateway: gateway.ok_or(Error::MissingGateway)?,
+            flags,
+            net_if: net_if.ok_or(Error::MissingInterface)?,
+            expires,
+       };
+       Ok(route)
     }
 }
 
